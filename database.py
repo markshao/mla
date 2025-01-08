9a8f44988d8ed4dc252f176cd3305f8e9afd636e
markshao <shaoyuliang@qq.com>
2025-01-08 08:19:06 +0800

    Revert "add jupyter nodebook"
    This reverts commit 767305af38e0bbbc9a776bba101399486a87359b.

--- database.py
+++ database.py
@@ -2,14 +2,10 @@ import os
 from sqlmodel import SQLModel, create_engine
 from models import *
 
-__engine = None
 sqldb = "mla.db"
 sqlite_url = f"sqlite:///{sqldb}"
 def get_engine(sql_url: str = None):
-    global __engine
-    if not __engine:
-        __engine = create_engine(sqlite_url)
-    return __engine
+    return create_engine(sqlite_url)
 
 def init_database_with_schema(sqlite_url: str = None):
     if os.path.exists(sqldb):
