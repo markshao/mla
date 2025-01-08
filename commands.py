767305af38e0bbbc9a776bba101399486a87359b
markshao <shaoyuliang@qq.com>
2025-01-08 08:18:18 +0800

    add jupyter nodebook

--- commands.py
+++ commands.py
@@ -10,7 +10,7 @@ from models import StockDayTrade
 @click.option("--tscode", default=None, help="stock code")
 def load_price(tscode=None):
     ts_client = get_client()
-    df = ts_client.daily(ts_code = tscode,start_date=yest_n_year_day(1),end_date=today())
+    df = ts_client.daily(ts_code = tscode,start_date=yest_n_year_day(3),end_date=today())
     with Session(get_engine()) as session:
         def action(t):
             s = StockDayTrade(**df.loc[t].to_dict())
--- database.py
+++ database.py
@@ -2,10 +2,14 @@ import os
 from sqlmodel import SQLModel, create_engine
 from models import *
 
+__engine = None
 sqldb = "mla.db"
 sqlite_url = f"sqlite:///{sqldb}"
 def get_engine(sql_url: str = None):
-    return create_engine(sqlite_url)
+    global __engine
+    if not __engine:
+        __engine = create_engine(sqlite_url)
+    return __engine
 
 def init_database_with_schema(sqlite_url: str = None):
     if os.path.exists(sqldb):
